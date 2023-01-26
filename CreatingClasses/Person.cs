562162629f6cb10bd1608861b943dc02f7a681a7
valkken <bryancolumbres@gmail.com>
2023-01-25 20:07:23 -0700

    Add project files.

---
+++ CreatingClasses/Person.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingClasses
+{
+    /// <summary>
+    /// This is the person class that defines several attributes
+    /// <remarks>Name: Bryan Jake Columbres (Jake)</remarks>
+    /// <remarks>Date: Jan 25, 2023</remarks>
+    /// </summary>
+    internal class Person
+    {
+        private int personId;
+        private string firstName;
+        private string lastName;
+        private string favoriteColor;
+        private int age;
+        private bool isWorking;
+
+        /// <summary>
+        /// Constructor for the person class with the following 5 attributes.
+        /// </summary>
+        /// <param name="personId"></param>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="favoriteColor"></param>
+        /// <param name="isWorking"></param>
+        public Person(int personId, string firstName, string lastName, string favoriteColor,int age, bool isWorking) 
+        { 
+            this.personId = personId;
+            this.firstName = firstName; 
+            this.lastName = lastName;   
+            this.favoriteColor = favoriteColor;
+            this.age = age; 
+            this.isWorking = isWorking;
+        } 
+
+        /// <summary>
+        /// method to print the person's first name, last name and favorite color
+        /// in a sentence format
+        /// </summary>
+        public void DisplayPersonInfo()
+        {
+            Console.WriteLine($"{this.personId}: {this.firstName} {this.lastName}'s favorite color is {this.favoriteColor}");
+        }
+        
+        /// <summary>
+        /// Method to changethe private fields of the person's favorite color to "white"
+        /// </summary>
+        public void ChangeFavoriteColor()
+        {
+            this.favoriteColor = "white";
+        }
+
+        /// <summary>
+        /// Method to calculate the age of the person and print it
+        /// </summary>
+        public void GetAgeInTenYears()
+        {
+            int ageTenYears = this.age + 10;
+            Console.WriteLine($"{this.firstName}'s age in 10 years is {ageTenYears}.");
+        }
+
+        /// <summary>
+        /// Overriden string method to display the person's information as a list (Not a traditional list, but as per document)
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString() 
+        {
+            return $"Person ID:{this.personId}\nFirstName: {this.firstName}\nLastName: {this.lastName}\nFavoriteColor: {this.favoriteColor}\nAge: {this.age}\nIsWorking:{this.isWorking}";
+        }
+     
+
+    }
+}
---
+++ CreatingClasses/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CreatingClasses
+{
+    /// <summary>
+    /// This is the main program that will create person objects
+    /// <remarks>Name: Bryan Jake Columbres (Jake)</remarks>
+    /// <remarks>Date: Jan 25, 2023</remarks>
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //a
+            //Create four objects using the following data
+            Person person1 = new Person(1, "Ian","Brooks", "Red", 30, true);
+            Person person2 = new Person(2, "Gina", "James", "Red", 18, false);
+            Person person3 = new Person(3, "Mike", "Briscoe", "Blue", 45, true);
+            Person person4 = new Person(4, "Mary", "Beals", "Yellow", 28, true);
+
+            //b
+            // Display's Gina's information as a sentence that shows her id, first name, last name, and her favorite color
+            person2.DisplayPersonInfo();
+
+            //c
+            //Display all of Mike's information as a list.
+            Console.WriteLine(person3.ToString()); 
+
+            //d
+            //Change Ian's favorite color to white, and then print his information as a sentence
+            person1.ChangeFavoriteColor();  
+            person1.DisplayPersonInfo();   
+            
+            //e
+            //Display Mary's age after 10 years
+            person4.GetAgeInTenYears(); 
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+            
+        }   
+    }
+}
